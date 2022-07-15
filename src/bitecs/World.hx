24166c611d7a21aed0b57ea74389fbe1b7a5e769
Antriel <Antriel@users.noreply.github.com>
2022-07-15 09:23:11 +0200

    cleanup

--- src/bitecs/World.hx
+++ src/bitecs/World.hx
@@ -1,13 +1,10 @@
 package bitecs;
 
+// TODO Consider changing to simpler IWorld auto build macro?
 @:genericBuild(bitecs.World.build()) class World<Rest> {
 
     public function new(?size:Int) {
         Bitecs.createWorld(this, size);
-    }
-
-    public function foo() {
-        return macro "foo";
     }
 
 }
