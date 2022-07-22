6de6063b3ff6636b58029f01c5b608d88aa6f235
Antriel <Antriel@users.noreply.github.com>
2022-07-22 13:31:51 +0200

    add `Entity.NONE`

--- src/bitecs/Entity.hx
+++ src/bitecs/Entity.hx
@@ -1,3 +1,9 @@
 package bitecs;
 
-abstract Entity(Int) to Int { }
+abstract Entity(Int) to Int {
+
+    public static inline var NONE:Entity = cast -1;
+
+    public inline function isValid():Bool return this >= 0;
+
+}
