494bc385e4dfab387c02689cbd37c91415b7d522
Antriel <Antriel@users.noreply.github.com>
2022-08-09 14:53:09 +0200

    query; fix sorting on expression that isn't a variable

--- src/bitecs/QueryExtensions.hx
+++ src/bitecs/QueryExtensions.hx
@@ -11,15 +11,16 @@ class QueryExtensions {
 
     public static macro function sort(iter:ExprOf<QueryIter>, expr:Expr) {
         var func = macro function(aE, bE) {
-            var a = $iter.getWrapper(aE);
-            var b = $iter.getWrapper(bE);
+            var a = iter.getWrapper(aE);
+            var b = iter.getWrapper(bE);
             return $expr;
         }
         var e = iter.field('ents').field('sort').call([func]);
         var t = Context.typeof(iter).toComplex();
         return macro {
-            $e;
-            ($iter:$t);
+            var iter = $iter;
+            iter.ents.sort($func);
+            (iter:$t);
         };
     }
 
